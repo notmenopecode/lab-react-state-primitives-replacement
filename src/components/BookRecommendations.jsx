b0b6deca9d4659997b965a1371a3efcf09120b6b
jd richards <jdrichardstech@gmail.com>
2023-12-07 19:16:35 -0500

    add suggestions

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -7,8 +7,9 @@ const BookRecommendations = () => {
   // State for all book data
   const [books, setBooks] = useState(bookData);
 
+  // SUGGESTED TODOS:
   // TODO: Implement state for selected genre and recommendations
-
+  // TODO: Implement state for recommendations
   // TODO: Implement the handleGenreSelect function
 
   return (
